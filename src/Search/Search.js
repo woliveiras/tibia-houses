34bc8e799fad5378fd822da96bba34df2be9c3c6
woliveiras <w.oliveira542@gmail.com>
2020-09-19 09:15:09 -0300

    chore(Domains): linting

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { housesEndpoint } from "./Search.config"
 import { useSearch } from './Context';
