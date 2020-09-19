c62d27f20d930bba7ba4a579f3324e01adcf81ed
woliveiras <w.oliveira542@gmail.com>
2020-09-19 11:26:03 -0300

    build(lint): add babel-parser

--- src/Search/Filters.js
+++ src/Search/Filters.js
@@ -3,54 +3,55 @@ import React, { useEffect, useState } from 'react'
 import { useSearch } from './Context'
 
 export const Filters = props => {
-    const { searchParams, setSearchParams } = useSearch()
-    const { selectedWorld } = searchParams
-    const [worlds, setWorlds] = useState([])
+  const { searchParams, setSearchParams } = useSearch()
+  const { selectedWorld } = searchParams
+  const [worlds, setWorlds] = useState([])
 
-    useEffect(() => {
-        async function fetchData() {
-            await fetch('https://api.tibiadata.com/v2/worlds.json')
-                .then(response => response.json())
-                .then(data => {
-                    setWorlds(data?.worlds?.allworlds)
-                })
-            }
-
-        fetchData()
-    }, [])
-
-    useEffect(() => {
-        setSearchParams({
-            ...searchParams,
-            worlds: worlds
+  useEffect(() => {
+    async function fetchData () {
+      await fetch('https://api.tibiadata.com/v2/worlds.json')
+        .then(response => response.json())
+        .then(data => {
+          setWorlds(data?.worlds?.allworlds)
         })
-        // eslint-disable-next-line
+    }
+
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    setSearchParams({
+      ...searchParams,
+      worlds: worlds
+    })
+    // eslint-disable-next-line
     }, [worlds])
 
-    const buildWorldList = worldsList => {
-        let worldsNamesList = []
+  const buildWorldList = worldsList => {
+    const worldsNamesList = []
 
-        for (const world in worldsList) {
-            worldsNamesList.push(worldsList[world].name)
-        }
-
-        return (
-            <select 
-                value={selectedWorld} 
-                onChange={
-                    event => 
-                        setSearchParams({ 
-                            ...searchParams, 
-                            selectedWorld: event.currentTarget.value
-                        })
-                }>
-                    <option>Selecione o mundo</option>
-                    {worldsNamesList.map(world => (<option key={world}>{ world }</option>))}
-            </select>
-        )
+    for (const world in worldsList) {
+      worldsNamesList.push(worldsList[world].name)
     }
 
     return (
-        <>{ worlds && buildWorldList(worlds) }</>
+      <select
+        value={selectedWorld}
+        onChange={
+          event =>
+            setSearchParams({
+              ...searchParams,
+              selectedWorld: event.currentTarget.value
+            })
+        }
+      >
+        <option>Selecione o mundo</option>
+        {worldsNamesList.map(world => (<option key={world}>{world}</option>))}
+      </select>
     )
+  }
+
+  return (
+    <>{worlds && buildWorldList(worlds)}</>
+  )
 }
--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,57 +1,57 @@
 import React, { useEffect, useState } from 'react'
 
-import { housesEndpoint } from "./Search.config"
+import { housesEndpoint } from './Search.config'
 import { useSearch } from './Context'
 
 export const Search = () => {
-    const { searchParams, setSearchParams } = useSearch()
-    const { selectedWorld } = searchParams
-    const [ housesData, setHousesData ] = useState([])
+  const { searchParams, setSearchParams } = useSearch()
+  const { selectedWorld } = searchParams
+  const [housesData, setHousesData] = useState([])
 
-    useEffect(() => {
-        async function fetchData() {
-            await fetch(`${housesEndpoint}${selectedWorld}.json`)
-                .then(response => response.json())
-                .then(data => {
-                    setHousesData(data.houses)
-                })
-            }
-
-        fetchData()
-    }, [selectedWorld])
-
-    useEffect(() => {
-        setSearchParams({
-            ...searchParams,
-            houses: housesData,
-            town: housesData?.town
+  useEffect(() => {
+    async function fetchData () {
+      await fetch(`${housesEndpoint}${selectedWorld}.json`)
+        .then(response => response.json())
+        .then(data => {
+          setHousesData(data.houses)
         })
-        // eslint-disable-next-line
+    }
+
+    fetchData()
+  }, [selectedWorld])
+
+  useEffect(() => {
+    setSearchParams({
+      ...searchParams,
+      houses: housesData,
+      town: housesData?.town
+    })
+    // eslint-disable-next-line
     }, [housesData])
 
-    return (
-        <div>
-            {housesData && (
-                <>
-                    <h2>Mundo: { searchParams.selectedWorld }</h2>
-                    <main>
-                        <header>
-                            <h2>Cidade: { searchParams.town }</h2>
-                        </header>
-                        <ul>
-                            { searchParams?.houses?.houses?.map(house => 
-                                <li key={ house.name }>
-                                    <div>
-                                        <p><strong>Nome: </strong>{ house.name }</p>
-                                        <p><strong>Preço: </strong>{ house.rent }</p>
-                                        <p><strong>Status: </strong>{ house.status }</p>
-                                    </div>
-                                </li>
+  return (
+    <div>
+      {housesData && (
+        <>
+          <h2>Mundo: {searchParams.selectedWorld}</h2>
+          <main>
+            <header>
+              <h2>Cidade: {searchParams.town}</h2>
+            </header>
+            <ul>
+              {searchParams?.houses?.houses?.map(house =>
+                <li key={house.name}>
+                  <div>
+                    <p><strong>Nome: </strong>{house.name}</p>
+                    <p><strong>Preço: </strong>{house.rent}</p>
+                    <p><strong>Status: </strong>{house.status}</p>
+                  </div>
+                </li>
                             )}
-                        </ul>
-                    </main>
-                </>
-            )}
-        </div>
-    )
+            </ul>
+          </main>
+        </>
+      )}
+    </div>
+  )
 }
