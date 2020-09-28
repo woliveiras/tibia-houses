a982c5a389609ba3da1269a5218362397f35559a
Weslley Henrique Cavalcante <whenriquerc@gmail.com>
2020-09-28 11:21:36 -0300

    react-gtm-module (#4)
    * feat: add react-gtm-module
    
    * feat: config to react-gtm-module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import TagMamager from 'react-gtm-module'
 import App from './App'
 
 import 'fontsource-roboto'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core/styles'
 import theme from './theme'
+import gtmConfig from './gtmConfig'
+
+TagMamager.initialize(gtmConfig)
 
 ReactDOM.render(
 
